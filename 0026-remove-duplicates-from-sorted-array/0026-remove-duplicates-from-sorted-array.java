1c6af224dea4aaa2e8dcb9de4f69221b55733101
Kenzo Tan <103299973+Kenzothd@users.noreply.github.com>
2023-02-24 19:30:48 +0800

    Time: 1 ms (100.00%), Space: 43.8 MB (82.25%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,14 +1,18 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        //unique count = 0 
         int i = 0;
         
         for(int j = 1; j < nums.length; j++) {
+            //if prev != current 
             if(nums[j]  != nums[i]) {
+                //add unique count by 1 and replace prev with current 
                 i++;
                 nums[i] = nums[j];
             }
         }
         
+        //return unique count
         return i + 1;
     }
 }
